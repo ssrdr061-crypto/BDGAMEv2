f904cc5ab88a674fbe03d6a7199fb3b21cb5c881
ssrdr061-crypto <ssrdr061@gmail.com>
2026-08-15 12:25:02 +0300

    Add files via upload

--- tema.js
+++ tema.js
@@ -4694,7 +4694,7 @@ document.head.appendChild(st);
     "#panel-inventory .inv-row{" +
     "  box-shadow:0 2px 6px rgba(0,20,45,.3) !important;" +
     "  position:relative !important;" +
-    "  padding-bottom:26px !important;" +
+    "  padding-bottom:28px !important;" +
     "  overflow:hidden !important;" +
     "}" +
     /* adet kutucuğun İÇİNDE, altta ortalı */
